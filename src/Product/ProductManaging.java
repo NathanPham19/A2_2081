e9728b08ded97aa480e00d6abf36ae657080af77
TUONG_TRAN <84768253+S3878734@users.noreply.github.com>
2023-01-16 15:21:12 +0700

    Update ProductManaging.java

--- src/Product/ProductManaging.java
+++ src/Product/ProductManaging.java
@@ -2,6 +2,7 @@ package Product;
 
 import java.io.IOException;
 
+//Resources use grom Lecture and Turorial slide and Stackoverflow
 public class ProductManaging {
     public static void main(String[] args) throws IOException {
         ProductControl pc = new ProductControl();
